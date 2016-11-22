209db922174061ec5f7a591edc65ab510d4ce1f6
SelinaChen07 <yingnachen@gmail.com>
2016-11-22 11:37:13 +1100

    Update full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		if page_title.empty?
+			full_title = "Ruby on Rails Tutorial Sample App"
+		else
+			full_title = "#{page_title} | Ruby on Rails Tutorial Sample App"
+		end
+	end
+
 end
